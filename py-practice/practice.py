21ba13618bce750c0fab8e10d398052128259c22
Nils Pukropp <nils@nirusu.codes>
2022-02-18 22:07:40 +0100

    update test for pythagorean_triples

--- py-practice/practice.py
+++ py-practice/practice.py
@@ -1,11 +1,13 @@
 from dataclasses import dataclass
 from typing import Optional
 
+
 @dataclass
 class Node:
     mark: str
     left: Optional['Node']
     right: Optional['Node']
+
 
 def find_substrings(node: Node, substr: str) -> list[str]:
     subs: list[str] = []
@@ -17,13 +19,16 @@ def find_substrings(node: Node, substr: str) -> list[str]:
         subs += find_substrings(node.right, substr)
     return subs
 
+
 def test_subs():
     tree = Node("aab", Node("baa", None, None), Node("aba", None, None))
     assert find_substrings(tree, 'ab') == ['aab', 'aba']
 
+
 def my_enumerate(s: str):
     for (i, c) in enumerate(s):
         yield (i, c)
+
 
 def prefixes(ls: list[int]):
     ls_n: list[int] = []
@@ -31,34 +36,38 @@ def prefixes(ls: list[int]):
     for i in ls:
         yield (ls_n := ls_n + [i])
 
+
 def alternate(xs, ys):
     while [x := next(xs, None), y := next(ys, None)] != [None, None]:
         if x is not None:
             yield x
         if y is not None:
             yield y
-        
 
 
 def test_enu():
-    assert list(my_enumerate("abcd")) == [(0, 'a'), (1, 'b'), (2, 'c'), (3, 'd')]
-    assert list(prefixes([1, 2, 3, 4])) == [[], [1], [1, 2], [1, 2, 3], [1, 2, 3, 4]]
-    assert list(alternate(iter("abcdef"), iter([0, 1, 2]))) == ['a', 0, 'b', 1, 'c', 2, 'd', 'e', 'f']
+    assert list(my_enumerate("abcd")) == [(0, 'a'), (1, 'b'), (2, 'c'),
+                                          (3, 'd')]
+    assert list(prefixes([1, 2, 3, 4])) == [[], [1], [1, 2], [1, 2, 3],
+                                            [1, 2, 3, 4]]
+    assert list(alternate(iter("abcdef"), iter(
+        [0, 1, 2]))) == ['a', 0, 'b', 1, 'c', 2, 'd', 'e', 'f']
+
 
 def twice(f):
     return lambda x: f(f(x))
 
+
 def test_twice():
     assert twice(lambda x: x * 2)(5) == 20
 
+
 def pythagorean_triples(n: int):
-    return [(a, b, c)   for a in range(1, n)
-                        for b in range(1, n)
-                        for c in range(1, n)
-                        if a**2 + b**2 == c**2 and a <= b]
+    return [(a, b, c) for a in range(1, n) for b in range(1, n)
+            for c in range(1, n) if a**2 + b**2 == c**2 and a <= b]
+
 
 def test_pyth():
-    assert list(pythagorean_triples(15)) == [ (3, 4, 5), (6, 8, 10), (5, 12, 13) ]
-
-
-
+    assert sorted(list(pythagorean_triples(15))) == sorted([(3, 4, 5),
+                                                            (6, 8, 10),
+                                                            (5, 12, 13)])
