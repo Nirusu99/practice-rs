524b6d7a929d9f6162718ce13aacf1d062107ad5
Nils Pukropp <nils@nirusu.codes>
2022-02-20 14:26:02 +0100

    format

--- py-practice/2020.py
+++ py-practice/2020.py
@@ -204,8 +204,10 @@ def test_twice():
 
 
 def pythagorean_triples(n: int):
-    return [(a, b, c) for a in range(1, n) for b in range(a, n)
-            for c in range(b, n) if a**2 + b**2 == c**2]
+    return [(a, b, c)   for a in range(1, n)
+                        for b in range(a, n)
+                        for c in range(b, n)
+                        if a**2 + b**2 == c**2]
 
 
 def test_pyth():
